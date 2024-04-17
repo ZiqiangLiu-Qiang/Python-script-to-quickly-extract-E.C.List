69f749cee2065ffd75c28a11c06f9438de7d128a
ZiqiangLiu-Qiang <167273977+ZiqiangLiu-Qiang@users.noreply.github.com>
2024-04-17 12:00:28 +0800

    extract_ec_from_keg.py

--- extract_ec_from_keg.py
+++ extract_ec_from_keg.py
@@ -1,30 +1,30 @@
 import os
 
 def extract_ec_from_keg(file_path):
-    with open(file_path, 'r') as keg_file:
-        content = keg_file.read()
-        # 使用正则表达式提取 [EC:...] 中的内容
-        ec_list = re.findall(r'\[EC:(.*?)\]', content)
-        # 将空格替换为换行符
-        ec_list = [ec.replace(' ', '\n') for ec in ec_list]
-        return ec_list
+     with open(file_path, 'r') as keg_file:
+         content = keg_file.read()
+         # Use regular expressions to extract the content in [EC:...]
+         ec_list = re.findall(r'\[EC:(.*?)\]', content)
+         # Replace spaces with newlines
+         ec_list = [ec.replace(' ', '\n') for ec in ec_list]
+         returnec_list
 
 def main():
-    folder_path = 'D:\\Qiang\\NJAU\\ModelConstruction\\EClist'  # EClist 文件夹路径
-    output_file = 'ECKEGGList.txt'  # 输出文件名
+     folder_path = 'D:\\EClist' # EClist folder path
+     output_file = 'ECKEGGList.txt' # Output file name
 
-    all_ecs = set()  # 用于存储所有提取的 EC
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.keg'):
-            file_path = os.path.join(folder_path, filename)
-            ecs = extract_ec_from_keg(file_path)
-            all_ecs.update(ecs)
+     all_ecs = set() # Used to store all extracted ECs
+     for filename in os.listdir(folder_path):
+         if filename.endswith('.keg'):
+             file_path = os.path.join(folder_path, filename)
+             ecs = extract_ec_from_keg(file_path)
+             all_ecs.update(ecs)
 
-    # 将结果写入文件
-    with open(output_file, 'w') as output:
-        for ec in sorted(all_ecs):
-            output.write(ec + '\n')
+     #Write results to file
+     with open(output_file, 'w') as output:
+         for ec in sorted(all_ecs):
+             output.write(ec + '\n')
 
 if __name__ == '__main__':
-    import re
-    main()
+     import re
+     main()
