e10f6aea2e0881067564449e104d837acb581a43
ZiqiangLiu-Qiang <167273977+ZiqiangLiu-Qiang@users.noreply.github.com>
2024-04-17 12:01:09 +0800

    extract_ec_from_keg2.py

--- extract_ec_from_keg2.py
+++ extract_ec_from_keg2.py
@@ -1,19 +1,19 @@
-# 读取原始文件
+# Read original file
 input_filename = "ECKEGGList.txt"
 output_filename = "XXECKEGGList.txt"
 
-# 读取文件内容
+# Read file content
 with open(input_filename, "r") as infile:
-    lines = infile.readlines()
+     lines = infile.readlines()
 
-# 去除带有“-”的行
+# Remove lines with "-"
 filtered_lines = [line for line in lines if "-" not in line]
 
-# 去除重复行
+# Remove duplicate rows
 unique_lines = list(set(filtered_lines))
 
-# 将结果写入新文件
+#Write results to new file
 with open(output_filename, "w") as outfile:
-    outfile.writelines(unique_lines)
+     outfile.writelines(unique_lines)
 
-print(f"处理完成，结果已保存到 {output_filename}")
+print(f"Processing completed, results have been saved to {output_filename}")
